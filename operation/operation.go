12fbf14ccf4cb9c6bccce38e8daf67e5acc41016
ashu01 <as01hu@gmail.com>
2016-06-28 18:59:13 +0530

    presence function updated

--- operation/operation.go
+++ operation/operation.go
@@ -2,17 +2,17 @@ package operation
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
-)
-
-var (
-	present bool
-	errs    error
-	index   int
 )
 
 //Presence to check presence
 func Presence(slice interface{}, val interface{}, startoptional ...int) (bool, int, error) {
+	var (
+		present bool
+		errs    error
+		index   int
+	)
 	sv := reflect.ValueOf(slice)
 	f := 0
 	present = false
@@ -20,23 +20,27 @@ func Presence(slice interface{}, val interface{}, startoptional ...int) (bool, i
 	End := sv.Len()
 	Start := 0
 	errs = nil
-	if len(startoptional) == 1 {
+	if len(startoptional) == 0 {
+		Start = 0
+	} else if len(startoptional) == 1 {
 		// fmt.Println(len(startoptional))
 		Start = startoptional[0]
 	} else if len(startoptional) == 2 {
 		Start = startoptional[0]
-		End = startoptional[1]
+		if startoptional[1] <= End {
+			End = startoptional[1]
+		}
 	} else {
 		errs = errors.New("You can't use more than 4 aurguments in presence function")
 		return false, -1, errs
 	}
 	// Check for first argument that must be slice or array
 	if reflect.TypeOf(slice).Kind() != reflect.Array && reflect.TypeOf(slice).Kind() != reflect.Slice {
-		errs = errors.New("array parameter's type is neither array nor slice\n")
+		errs = errors.New("array parameter's type is neither array nor slice")
 	}
 	// check for type of val is same as that of slice element
 	if reflect.TypeOf(slice).Elem() != reflect.TypeOf(val) {
-		errs = errors.New("mismatch of slice type and item type is found \n")
+		errs = errors.New("mismatch of slice type and item type is found ")
 	}
 	// code for checking presence of element in given slice
 	// time complexity O(N) and space complexity O(1)
@@ -63,6 +67,12 @@ func DeleteEle(slice interface{}, item interface{}, optional ...int) (interface{
 	var err error
 	Start := 0
 	End := sv.Len()
+
+	// fmt.Printf("%T\n", slice)
+	// var r_list []interface{}
+	// r_list := make([]reflect.TypeOf(item),0)
+	// xx := reflect.TypeOf(item)
+	// fmt.Printf(xx)
 	// Check for first argument that must be slice or array
 	if reflect.TypeOf(slice).Kind() != reflect.Array && reflect.TypeOf(slice).Kind() != reflect.Slice {
 		err = errors.New("array parameter's type is neither array nor slice\n")
@@ -71,9 +81,37 @@ func DeleteEle(slice interface{}, item interface{}, optional ...int) (interface{
 	if reflect.TypeOf(slice).Elem() != reflect.TypeOf(item) {
 		err = errors.New("mismatch of slice type and item type is found \n")
 	}
-
+	// fmt.Println("item : ", item)
+	fmt.Printf("%T\n", item)
+	j := 0
 	for i := Start; i < End; i++ {
-
+		// fmt.Printf("i : %T \n", sv.Index(i).Interface())
+		if sv.Index(i).Interface() == item {
+			continue
+		} else {
+			tmp := sv.Index(i).Interface()
+			fmt.Println(tmp)
+			// r_list = append(r_list, tmp)
+			// sv.Index(j).Interface() = tmp
+			j++
+		}
 	}
-	return sv, End, err
+	return 1, 2, err
+	// return r_list.([]reflect.TypeOf(item)), End, err
+}
+
+//PopBack pops the last element from slice/array and it returns new slice with the popped item and error
+func PopBack(slice interface{}) (interface{}, interface{}, error) {
+	sv := reflect.ValueOf(slice)
+	length := sv.Len()
+	item := sv.Index(length - 1).Interface()
+	// fmt.Printf("%T\n", item)
+	// fmt.Println(item)
+	var err error
+	// Check for first argument that must be slice or array
+	if reflect.TypeOf(slice).Kind() != reflect.Array && reflect.TypeOf(slice).Kind() != reflect.Slice {
+		err = errors.New("array parameter's type is neither array nor slice\n")
+	}
+
+	return 1, item, err
 }
